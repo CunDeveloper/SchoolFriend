9344347b68054bb0b1f80773a24a41dd8b706b8f
CunDeveloper <15261893862@163.com>
2015-12-07 09:18:05 +0800

    add schoolInfo file

---
+++ src/com/nju/model/SchoolInfo.java
@@ -0,0 +1,35 @@
+package com.nju.model;
+
+public class SchoolInfo {
+
+	private int id;
+	private String name;
+	private String iconUrl;
+	private String desc;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getIconUrl() {
+		return iconUrl;
+	}
+	public void setIconUrl(String iconUrl) {
+		this.iconUrl = iconUrl;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+}
